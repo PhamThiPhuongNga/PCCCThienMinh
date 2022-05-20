2da72b8c0338d574b798a63cc9647e3b6e9311d2
Your Name <you@example.com>
2022-05-20 23:25:58 +0700

    up 1130 20/5

---
+++ view/introduce.php
@@ -0,0 +1,103 @@
+<?php require_once ("../view/template/header.php")?>
+
+<!--Page Title-->
+<section class="page-title centred" style="background-image: url(/assets/images/background/page-title.jpg);">
+    <div class="overlay-bg"></div>
+    <div class="pattern-layer"></div>
+    <div class="auto-container">
+        <div class="content-box">
+            <div class="title">
+                <h1>GIỚI THIỆU CÔNG TY</h1>
+            </div>
+            <ul class="bread-crumb clearfix">
+                <li><a href="index.html">Trang chủ</a></li>
+                <li>Giới thiệu công ty</li>
+            </ul>
+        </div>
+    </div>
+</section>
+<!--End Page Title-->
+
+
+<!-- service-details -->
+<section class="service-details">
+    <div class="auto-container">
+        <div class="row clearfix">
+            <div class="col-lg-12 col-md-12 col-sm-12 content-side">
+                <div class="service-details-content">
+                    <div class="inner-box">
+                        <figure class="image-box"><img src="assets/images/service/service-9.jpg" alt=""></figure>
+                        <div class="text">
+                            <h2>GIỚI THIỆU CÔNG TY</h2>
+                            <p style="font-style:italic; color: red;">Kính gửi quý khách hàng và đối tác </p>
+                            <span>Được thành lập 27 tháng 02 năm 2019, <strong>CÔNG TY TNHH PHÒNG CHÁY CHỮA CHÁY THIÊN MINH</strong> với sứ mệnh mang đến những sản phẩm mang thương hiệu <strong>PCCC THIÊN MINH</strong> chất lượng nhất đến tay người tiêu dùng, phủ khắp tất cả các công trình xây dựng lớn nhỏ, 
+                                        cung cấp mọi dịch vụ lắp đặt, sửa chữa, bảo dưỡng bảo trì hệ thống các thiết bị phòng cháy chữa cháy Thiên Minh toàn quốc cùng với sứ mệnh đảm bảo an toàn tuyệt đối tính mạng và tài sản cho khách hàng về an toàn phòng cháy chữa cháy và cứu hộ
+                                         cứu nạn. </br>Với sứ mệnh đó, trong nhiều năm qua bằng việc tư vấn giải pháp, thiết kế, cung cấp trang thiết bị, phương tiện và giải pháp tích hợp tổng thể về an toàn phòng cháy chữa cháy
+                                        Với các thành viên giàu kinh nghiệm và uy tín đã có nhiều năm gắn bó với nghề, hiểu biết kỹ thuật chuyên sâu, nắm vững chuyên môn nghiệp vụ. 
+                                    </span></br>
+                            <div class="centred mt-4">
+                                <img src="/assets/images/LOGOTM.png" alt="" width="100px" height="130px">
+                                </br><span style="font-size: 13px;">Logo Công ty TNHH Phòng cháy chữa cháy Thiên Minh</span>
+                            </div>
+
+                            </br><span>Trong thời gian qua, công ty chúng tôi đã thi công nhiều công trình trong các lĩnh vực hoạt động với 
+                                        chất lượng cao mang tính chuyên nghiệp. 
+                                        </br>Mục tiêu hàng đầu của <strong>PCCC THIÊN MINH</strong> chúng tôi luôn nghiên cứu khả năng ứng dụng của các thiết bị trong quá trình tư vấn thiết kế, thi công các hạng mục Phòng cháy và chữa 
+                                        cháy nhằm đưa công trình vào hoạt động an toàn và hiệu quả nhất. Để thực hiện các mục tiêu trên Công ty chúng tôi đã tập hợp đội ngũ quản lý cán bộ tốt: kỹ sư có năng lực chuyên môn cao, 
+                                        nhân viên kinh doanh giỏi. Đồng thời chúng tôi luôn chú trọng khâu đào tạo để có một đội ngũ chuyên viên kỹ thuật cao, chuyên nghiệp.
+                                        </br>Với mong muốn đáp ứng tốt các yêu cầu của quý khách hàng chúng tôi muốn phục vụ với tất cả năng lực và uy tín của mình để đảm bảo hoàn thành tốt mọi yêu cầu về chất lượng tiến độ cũng như kỹ thuật với hiệu quả cao nhất.
+                                    </span>
+                            </br>
+                            </br>
+                            <!-- <p>It has survived not only five centuries. Lorem Ipsum is simply dummy text of the new design printng and type setting Ipsum take a look at our round. When an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting.</p> -->
+                        </div>
+                        <div class="two-column">
+                            <div class="row clearfix">
+                                <div class="col-lg-6 col-md-6 col-sm-12 image-column">
+                                    <figure class="image-box"><img src="/assets/images/resource/image.png" alt=""></figure>
+                                </div>
+                                <div class="col-lg-6 col-md-6 col-sm-12 content-column">
+                                    <div class="text">
+                                        <h4>LĨNH VỰC KIN DOANH</h4>
+                                        <ul class="list-item clearfix">
+                                            <li>Công ty TNHH Phòng cháy chữa cháy Thiên Minh hoạt động chính trong lĩnh vực cung cấp và thi công lắp đặt các phương tiện thiết bị và vật tư Phòng cháy chữa cháy:</li>
+                                            <li>Cung cấp phương tiện, thiết bị , vât tư Phòng cháy chữa cháy: Hệ thống báo cháy, hệ thống chữa cháy, hệ thống tăng áp & hút khói và hệ thống đèn Exit sự cố.</li>
+                                            <li>Tư vấn thiết kế, thẩm duyệt và tư vấn giám sát về Phòng cháy chữa cháy: Hồ sơ thiết kế cơ sở, Tư vấn giám sát và Kiểm định thiết bị Phòng cháy chữa cháy.</li>
+                                            <li>Thi công lắp đặt hệ thống Phòng cháy chữa cháy: Hệ thống báo cháy, hệ thống chữa cháy, hệ thống tăng áp hút khói và hệ thống đèn Exit sự cố.</li>
+                                            <li>Chúng tôi cam kết cung cấp cho khách hàng sản phẩm dịch vụ và sản phẩm tốt nhất, phù hợp nhất và với chi phí tiết kiệm nhất đẩm bảo sự hài lòng 100% của Quý khách hàng và Quý đôi tác.</li>
+                                            <li>Bằng sự tận tuỵ và tinh thần học hỏi không ngừng học hỏi, <strong>PCCC THIÊN MINH</strong>hy vọng tiếp tục gắn bó và trở thành người bạn đáng tin cậy nhất, luông đồng hành cùng sự phát triển bền vững của Quý
+                                                khách hàng và Quý đối tác.</li>
+                                            <li style="color: red;">GIAO HÀNG TẬN NƠI TRÊN TOÀN QUỐC- HỖ TRỢ TƯ VẤN VÀ BÁN HÀNG 24/24H. GỌI NGAY 0334972038 (ZALO/CALL)</li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        </br><span>Xin trân thành cảm ơn và rất mong nhận được sự hợp tác của Quý khách hàng và Quý đối tác.</span>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- service-details end -->
+
+
+<!-- cta-section -->
+<section class="cta-section bg-color-3">
+    <div class="pattern-layer" style="background-image: url(assets/images/shape/pattern-1.png);"></div>
+    <div class="auto-container">
+        <div class="inner-box clearfix">
+            <div class="text pull-left">
+                <h2>24/7 Available. Call us Immediately<br /><a href="tel:6668880000">666 888 0000</a></h2>
+            </div>
+            <div class="btn-box pull-right">
+                <a href="contact.html" class="theme-btn-one">Contact with us</a>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- cta-section end -->
+
+<?php require_once("../view/template/footer.php") ?>
--- view/template/footer.php
+++ view/template/footer.php
@@ -117,17 +117,17 @@
 
     <!-- jequery plugins -->
     <!-- <script src="assets/js/jquery.js"></script> -->
-    <script src="assets/js/popper.min.js"></script>
-    <script src="assets/js/bootstrap.min.js"></script>
-    <script src="assets/js/owl.js"></script>
-    <script src="assets/js/wow.js"></script>
-    <script src="assets/js/validation.js"></script>
-    <script src="assets/js/jquery.fancybox.js"></script>
-    <script src="assets/js/appear.js"></script>
-    <script src="assets/js/jquery.countTo.js"></script>
+    <script src="/assets/js/popper.min.js"></script>
+    <script src="/assets/js/bootstrap.min.js"></script>
+    <script src="/assets/js/owl.js"></script>
+    <script src="/assets/js/wow.js"></script>
+    <script src="/assets/js/validation.js"></script>
+    <script src="/assets/js/jquery.fancybox.js"></script>
+    <script src="/assets/js/appear.js"></script>
+    <script src="/assets/js/jquery.countTo.js"></script>
 
     <!-- main-js -->
-    <script src="assets/js/script.js"></script>
+    <script src="/assets/js/script.js"></script>
 
 </body>
 <!-- End of .page_wrapper -->
